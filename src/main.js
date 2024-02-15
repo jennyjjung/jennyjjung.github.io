e404973431457641699f24043fa8c1ebd075b282
Jenny Jung <84524297+jennyjjung@users.noreply.github.com>
2024-02-14 21:08:58 -0500

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// window = global object (브라우저에서 기본적으로 제공하는 객체 - 삭제가능)
+// window.document.querySelector
+
+
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+const header = document.querySelector('.header');
+// const headerRect = header.getBoundingClientRect();
+// console.log(headerRect);
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  // 스크롤되는 Y 좌표가 headerHeight보다 크다면 다른 스타일링
+  const y = window.scrollY;
+  if (y > headerHeight) {
+    // .header 클래스에 .header--dark 라는 클래스 추가
+    header.classList.add('header--dark'); 
+  } else {
+    // .header 클래스에 .header--dark 라는 클래스 제거
+    header.classList.remove('header--dark');
+  }
+})
